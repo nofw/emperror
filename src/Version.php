f43b046240c0082fc4d7b2f25d2e42390f00f4b4
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-04-26 13:33:20 +0200

    Add Phpbrake implementation
    Signed-off-by: Márk Sági-Kazár <mark.sagikazar@gmail.com>

---
+++ src/Version.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nofw\Emperror;
+
+/**
+ * Version interface provides version information.
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+interface Version
+{
+    public const VERSION = '0.1.0-DEV';
+}
